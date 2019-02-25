a0afc22eaaeec874578dcd70013384ea9f858399
shenwu <359618410@qq.com>
2019-02-25 17:23:10 +0800

    fix

---
+++ build/precache-manifest.16d503e3ff2db89406878e63cdcea2d7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "51290cd4c0e1bcb6b510",
+    "url": "/static/js/main.51290cd4.chunk.js"
+  },
+  {
+    "revision": "418fa4b7e52d698475ab",
+    "url": "/static/js/2.418fa4b7.chunk.js"
+  },
+  {
+    "revision": "51290cd4c0e1bcb6b510",
+    "url": "/static/css/main.8fedcf9d.chunk.css"
+  },
+  {
+    "revision": "418fa4b7e52d698475ab",
+    "url": "/static/css/2.ee5fa052.chunk.css"
+  },
+  {
+    "revision": "de0e717b9b3359fe42073f90c50e0b4f",
+    "url": "/index.html"
+  }
+];
